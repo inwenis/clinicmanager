cd24c49217f9cd12a4501808d88d6c01317ef655
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-09-16 20:47:44 +0200

    initial commit

---
+++ ClinicManager/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace ClinicManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            PatientsListBox.ItemsSource = LoadFromFile();
+        }
+
+        private Patient[] LoadFromFile()
+        {
+            var allText = File.ReadAllText("samplePatients.json");
+            var jsonSerializer = JsonSerializer.Create(new JsonSerializerSettings()
+            {
+                DateFormatString = "dd/MM/yyyy"
+            });
+            var patients = jsonSerializer.Deserialize<List<Patient>>(new JsonTextReader(new StringReader(allText)));
+            for (var i = 0; i < patients.Count; i++)
+            {
+                var patient = patients[i];
+                patient.Photo = GetPhotoForUser(patient);
+            }
+
+            return patients.ToArray();
+        }
+
+        private static string GetPhotoForUser(Patient patient)
+        {
+            return patient.InsuranceNumber.Last() % 2 == 0 ? "Photos/male.png" : "Photos/female.png";
+        }
+
+        private void PatientsListBox_OnSelected(object sender, RoutedEventArgs e)
+        {
+            EditButton.IsEnabled = true;
+            var selectedPatient = (Patient) ((ListBox) sender).SelectedItem;
+            NameTextBox.Text = selectedPatient.FirstName + " " + selectedPatient.SecondName;
+            EmailTextBox.Text = selectedPatient.Email;
+            PhoneTextBox.Text = selectedPatient.PhoneNumber;
+            AgeTextBox.Text = ((DateTime.Now - selectedPatient.BirthDate).TotalDays / 365).ToString();
+            InsuranceNumberTextBox.Text = selectedPatient.InsuranceNumber;
+            Photo.Source = new BitmapImage(new Uri(selectedPatient.Photo, UriKind.Relative));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            PatientDetailView detailView = new PatientDetailView();
+            detailView.Patient = ((Patient) PatientsListBox.SelectedItem);
+            detailView.ShowDialog();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            PatientDetailView detailView = new PatientDetailView();
+            detailView.Patient = ((Patient) PatientsListBox.SelectedItem);
+            detailView.ShowDialog();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            var jsonSerializer = JsonSerializer.Create(new JsonSerializerSettings()
+            {
+                DateFormatString = "dd/MM/yyyy"
+            });
+            File.Delete("samplePatients.json");
+            using (var streamWriter = new StreamWriter(File.OpenWrite("samplePatients.json")))
+            {
+                jsonSerializer.Serialize(streamWriter, PatientsListBox.Items);
+            }
+        }
+    }
+}
---
+++ ClinicManager/PatientDetailView.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClinicManager
+{
+    /// <summary>
+    /// Interaction logic for PatientDetailView.xaml
+    /// </summary>
+    public partial class PatientDetailView : Window
+    {
+        public Patient Patient { get; set; }
+
+        public PatientDetailView()
+        {
+            InitializeComponent();
+            this.Loaded += DetailView_Loaded;
+        }
+
+        void DetailView_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            NameLabel.Content = Patient.FirstName + " " + Patient.SecondName;
+            PhoneTextBox.Text = Patient.PhoneNumber;
+            EmailTextBox.Text = Patient.Email;
+            AgeTextBox.Text = (2016 - Patient.BirthDate.Year).ToString();
+            Image.Source = new BitmapImage(new Uri(Patient.Photo, UriKind.Relative));
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}

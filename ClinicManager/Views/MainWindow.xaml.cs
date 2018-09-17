fcd758867f647814067c6bcaf280a2fd668e7725
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-09-17 08:10:23 +0200

    remove unnecessary usings

--- ClinicManager/Views/MainWindow.xaml.cs
+++ ClinicManager/Views/MainWindow.xaml.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media.Imaging;
 using ClinicManager.Models;
 using ClinicManager.ViewModels;
 using Newtonsoft.Json;

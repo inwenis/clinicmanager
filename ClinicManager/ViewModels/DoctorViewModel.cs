2bda5f07a73ef7ae63f676888f74c603abcf31ee
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-10-02 08:35:24 +0200

    creating an empty image was throwing an exception

--- ClinicManager/ViewModels/DoctorViewModel.cs
+++ ClinicManager/ViewModels/DoctorViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -128,7 +129,27 @@ namespace ClinicManager.ViewModels
                 }
                 else
                 {
-                    return new BitmapImage();
+                    int width = 128;
+                    int height = width;
+                    int stride = width/8;
+                    byte[] pixels = new byte[height*stride];
+
+                    // Try creating a new image with a custom palette.
+                    List<System.Windows.Media.Color> colors = new List<System.Windows.Media.Color>();
+                    colors.Add(System.Windows.Media.Colors.Red);
+                    colors.Add(System.Windows.Media.Colors.Blue);
+                    colors.Add(System.Windows.Media.Colors.Green);
+                    BitmapPalette myPalette = new BitmapPalette(colors);
+
+                    // Creates a new empty image with the pre-defined palette
+                    BitmapSource image = BitmapSource.Create(
+                        width, height,
+                        96, 96,
+                        PixelFormats.Indexed1,
+                        myPalette, 
+                        pixels, 
+                        stride);
+                    return image;
                 }
             }
         }
